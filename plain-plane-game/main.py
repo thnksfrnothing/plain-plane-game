2ab574d50a69557d112d320befcc172731dda03f
thnksfrnothing <eternalreward420@gmail.com>
2025-04-14 08:32:57 +0000

    plainplanegame.py
    plain plane game update 1.0
       --------------------------------
    - made player hitbox more forgivable
    - enlarged enemy hitboxs
    - added a second bullet
    - resized window and adjusted assets
    - optimization
    - player bullets now stay in screen
    - red enemy ship flies in <= score 15 now
    - bullets no longer follow player or enemy x axis

--- plain-plane-game/main.py
+++ plain-plane-game/main.py
@@ -1,12 +1,8 @@
+
+
 import pygame
 import random
-import math
 
-#def music_start():
-#	if move == True: 
-#		pygame.mixer.music.load('audioassets/runninginthe.wav')
-#	if move == False:
-#		pygame.mixer.music.stop()
 
 def reset_to_top():
 	rand_genx = random.randint(0, WIDTH)
@@ -148,56 +144,121 @@ def reset_to_top():
 		lake.y = rand_geny
 
 def move_background():
-	ground_scroll_speed = 8
-	cloud_scroll_speed = 5.5
-	key = pygame.key.get_pressed()
-	rock1.move_ip(0, ground_scroll_speed)
-	rock2.move_ip(0, ground_scroll_speed)
-	bush1.move_ip(0, ground_scroll_speed)
-	bush2.move_ip(0, ground_scroll_speed)
-	bush3.move_ip(0, ground_scroll_speed)
-	bush4.move_ip(0, ground_scroll_speed)
-	house.move_ip(0, ground_scroll_speed)
-	house2.move_ip(0, ground_scroll_speed)
-	house3.move_ip(0, ground_scroll_speed)
-	house4.move_ip(0, ground_scroll_speed)
-	lake.move_ip(0, ground_scroll_speed)
-	cloud_p1.move_ip(0, cloud_scroll_speed)
-	cloud_p2.move_ip(0, cloud_scroll_speed)
-	cloud2_p1.move_ip(0, cloud_scroll_speed)
-	cloud2_p2.move_ip(0, cloud_scroll_speed)
-	if key[pygame.K_a]:
-		rock1.move_ip(ground_scroll_speed, 0)
-		rock2.move_ip(ground_scroll_speed, 0)
-		bush1.move_ip(ground_scroll_speed, 0)
-		bush2.move_ip(ground_scroll_speed, 0)
-		bush3.move_ip(ground_scroll_speed, 0)
-		bush4.move_ip(ground_scroll_speed, 0)
-		house.move_ip(ground_scroll_speed, 0)
-		house2.move_ip(ground_scroll_speed, 0)
-		house3.move_ip(ground_scroll_speed, 0)
-		house4.move_ip(ground_scroll_speed, 0)
-		lake.move_ip(ground_scroll_speed, 0)
-		cloud_p1.move_ip(cloud_scroll_speed, 0)
-		cloud_p2.move_ip(cloud_scroll_speed, 0)
-		cloud2_p1.move_ip(cloud_scroll_speed, 0)
-		cloud2_p2.move_ip(cloud_scroll_speed, 0)
-	if key[pygame.K_d]:
-		rock1.move_ip(-ground_scroll_speed, 0)
-		rock2.move_ip(-ground_scroll_speed, 0)
-		bush1.move_ip(-ground_scroll_speed, 0)
-		bush2.move_ip(-ground_scroll_speed, 0)
-		bush3.move_ip(-ground_scroll_speed, 0)
-		bush4.move_ip(-ground_scroll_speed, 0)
-		house.move_ip(-ground_scroll_speed, 0)
-		house2.move_ip(-ground_scroll_speed, 0)
-		house3.move_ip(-ground_scroll_speed, 0)
-		house4.move_ip(-ground_scroll_speed, 0)
-		lake.move_ip(-ground_scroll_speed, 0)
-		cloud_p1.move_ip(-cloud_scroll_speed, 0)
-		cloud_p2.move_ip(-cloud_scroll_speed, 0)
-		cloud2_p1.move_ip(-cloud_scroll_speed, 0)
-		cloud2_p2.move_ip(-cloud_scroll_speed, 0)
+	if move == True:
+		ground_scroll_speed = 8
+		cloud_scroll_speed = 5.5
+		key = pygame.key.get_pressed()
+		rock1.move_ip(0, ground_scroll_speed)
+		rock2.move_ip(0, ground_scroll_speed)
+		bush1.move_ip(0, ground_scroll_speed)
+		bush2.move_ip(0, ground_scroll_speed)
+		bush3.move_ip(0, ground_scroll_speed)
+		bush4.move_ip(0, ground_scroll_speed)
+		house.move_ip(0, ground_scroll_speed)
+		house2.move_ip(0, ground_scroll_speed)
+		house3.move_ip(0, ground_scroll_speed)
+		house4.move_ip(0, ground_scroll_speed)
+		lake.move_ip(0, ground_scroll_speed)
+		cloud_p1.move_ip(0, cloud_scroll_speed)
+		cloud_p2.move_ip(0, cloud_scroll_speed)
+		cloud2_p1.move_ip(0, cloud_scroll_speed)
+		cloud2_p2.move_ip(0, cloud_scroll_speed)
+		if key[pygame.K_a]:
+			rock1.move_ip(ground_scroll_speed, 0)
+			rock2.move_ip(ground_scroll_speed, 0)
+			bush1.move_ip(ground_scroll_speed, 0)
+			bush2.move_ip(ground_scroll_speed, 0)
+			bush3.move_ip(ground_scroll_speed, 0)
+			bush4.move_ip(ground_scroll_speed, 0)
+			house.move_ip(ground_scroll_speed, 0)
+			house2.move_ip(ground_scroll_speed, 0)
+			house3.move_ip(ground_scroll_speed, 0)
+			house4.move_ip(ground_scroll_speed, 0)
+			lake.move_ip(ground_scroll_speed, 0)
+			cloud_p1.move_ip(cloud_scroll_speed, 0)
+			cloud_p2.move_ip(cloud_scroll_speed, 0)
+			cloud2_p1.move_ip(cloud_scroll_speed, 0)
+			cloud2_p2.move_ip(cloud_scroll_speed, 0)
+		if key[pygame.K_d]:
+			rock1.move_ip(-ground_scroll_speed, 0)
+			rock2.move_ip(-ground_scroll_speed, 0)
+			bush1.move_ip(-ground_scroll_speed, 0)
+			bush2.move_ip(-ground_scroll_speed, 0)
+			bush3.move_ip(-ground_scroll_speed, 0)
+			bush4.move_ip(-ground_scroll_speed, 0)
+			house.move_ip(-ground_scroll_speed, 0)
+			house2.move_ip(-ground_scroll_speed, 0)
+			house3.move_ip(-ground_scroll_speed, 0)
+			house4.move_ip(-ground_scroll_speed, 0)
+			lake.move_ip(-ground_scroll_speed, 0)
+			cloud_p1.move_ip(-cloud_scroll_speed, 0)
+			cloud_p2.move_ip(-cloud_scroll_speed, 0)
+			cloud2_p1.move_ip(-cloud_scroll_speed, 0)
+			cloud2_p2.move_ip(-cloud_scroll_speed, 0)
+		if key[pygame.K_LEFT]:
+			rock1.move_ip(ground_scroll_speed, 0)
+			rock2.move_ip(ground_scroll_speed, 0)
+			bush1.move_ip(ground_scroll_speed, 0)
+			bush2.move_ip(ground_scroll_speed, 0)
+			bush3.move_ip(ground_scroll_speed, 0)
+			bush4.move_ip(ground_scroll_speed, 0)
+			house.move_ip(ground_scroll_speed, 0)
+			house2.move_ip(ground_scroll_speed, 0)
+			house3.move_ip(ground_scroll_speed, 0)
+			house4.move_ip(ground_scroll_speed, 0)
+			lake.move_ip(ground_scroll_speed, 0)
+			cloud_p1.move_ip(cloud_scroll_speed, 0)
+			cloud_p2.move_ip(cloud_scroll_speed, 0)
+			cloud2_p1.move_ip(cloud_scroll_speed, 0)
+			cloud2_p2.move_ip(cloud_scroll_speed, 0)
+		if key[pygame.K_RIGHT]:
+			rock1.move_ip(-ground_scroll_speed, 0)
+			rock2.move_ip(-ground_scroll_speed, 0)
+			bush1.move_ip(-ground_scroll_speed, 0)
+			bush2.move_ip(-ground_scroll_speed, 0)
+			bush3.move_ip(-ground_scroll_speed, 0)
+			bush4.move_ip(-ground_scroll_speed, 0)
+			house.move_ip(-ground_scroll_speed, 0)
+			house2.move_ip(-ground_scroll_speed, 0)
+			house3.move_ip(-ground_scroll_speed, 0)
+			house4.move_ip(-ground_scroll_speed, 0)
+			lake.move_ip(-ground_scroll_speed, 0)
+			cloud_p1.move_ip(-cloud_scroll_speed, 0)
+			cloud_p2.move_ip(-cloud_scroll_speed, 0)
+			cloud2_p1.move_ip(-cloud_scroll_speed, 0)
+			cloud2_p2.move_ip(-cloud_scroll_speed, 0)
+
+def bullet_two():
+	global score
+	if second_bullet.y > 0:
+		second_bullet.move_ip(0, -7)
+		if second_bullet.y <= 0:
+			second_bullet.x = plane_hitbox.x
+			second_bullet.y = plane_hitbox.y
+		if second_bullet.colliderect(enemy1_hitbox):
+			score += 1
+			pygame.mixer.Sound.play(hitsound)
+			pygame.mixer.music.stop()
+			enemy1_hitbox.x = random.randint(30, WIDTH - 30)
+			enemy1_hitbox.y = -350
+			second_bullet.x = plane_hitbox.x
+			second_bullet.y = plane_hitbox.y
+		if second_bullet.colliderect(enemy2_hitbox):
+			score += 1
+			pygame.mixer.Sound.play(hitsound)
+			pygame.mixer.music.stop()
+			enemy2_hitbox.x = random.randint(30, WIDTH - 30)
+			enemy2_hitbox.y = -250
+			second_bullet.x = plane_hitbox.x
+			second_bullet.y = plane_hitbox.y
+		if second_bullet.colliderect(enemy3_hitbox):
+			score += 1
+			pygame.mixer.Sound.play(hitsound)
+			pygame.mixer.music.stop()
+			enemy3_hitbox.x = random.randint(30, WIDTH - 30)
+			enemy3_hitbox.y = -150
+			second_bullet.x = plane_hitbox.x
+			second_bullet.y = plane_hitbox.y
 
 def enemy_movement():
 	global score  
@@ -206,7 +267,7 @@ def enemy_movement():
 	if score >= 10:
 		if enemy3_hitbox.y != 100:
 			enemy3_hitbox.move_ip(0, 5)
-	if score >= 20:
+	if score >= 15:
 		if enemy2_hitbox.y != 100:
 			enemy2_hitbox.move_ip(0, 5)
 	if score < 5:
@@ -222,20 +283,34 @@ def shoot_player():
 		enemy1_hitbox.move_ip(5, 0)
 		enemy2_hitbox.move_ip(5, 0)
 		enemy3_hitbox.move_ip(5, 0)
-		bullet.move_ip(2, 0)
-		bullet3.move_ip(2, 0)
-		bullet4.move_ip(2, 0)
+		bullet.move_ip(5, 0)
+		bullet3.move_ip(7, 0)
+		bullet4.move_ip(6, 0)
+	if key[pygame.K_LEFT]:
+		enemy1_hitbox.move_ip(5, 0)
+		enemy2_hitbox.move_ip(5, 0)
+		enemy3_hitbox.move_ip(5, 0)
+		bullet.move_ip(5, 0)
+		bullet3.move_ip(7, 0)
+		bullet4.move_ip(6, 0)
 	if key[pygame.K_d]:
 		enemy1_hitbox.move_ip(-5, 0)
 		enemy2_hitbox.move_ip(-5, 0)
 		enemy3_hitbox.move_ip(-5, 0)
-		bullet.move_ip(-2, 0)
-		bullet3.move_ip(-2, 0)
-		bullet4.move_ip(-2, 0)
+		bullet.move_ip(-5, 0)
+		bullet3.move_ip(-7, 0)
+		bullet4.move_ip(-6, 0)
+	if key[pygame.K_RIGHT]:
+		enemy1_hitbox.move_ip(-5, 0)
+		enemy2_hitbox.move_ip(-5, 0)
+		enemy3_hitbox.move_ip(-5, 0)
+		bullet.move_ip(-5, 0)
+		bullet3.move_ip(-7, 0)
+		bullet4.move_ip(-6, 0)
 	if move == True:
-		if bullet.y != WIDTH + 20:
+		if bullet.y != HEIGHT:
 			bullet.move_ip(0, 5)
-	if bullet.y >= WIDTH:
+	if bullet.y >= HEIGHT:
 		bullet.x = enemy1_hitbox.x + 20
 		bullet.y = enemy1_hitbox.y + 100
 	if bullet.colliderect(plane_hitbox) or bullet.colliderect(plane_hitbox_y):
@@ -246,10 +321,10 @@ def shoot_player():
 		bullet.y = enemy1_hitbox.y + 100
 		if health == 0:
 			move = False
-	if score >= 20:
-		if bullet3.y != WIDTH + 20:
+	if score >= 15:
+		if bullet3.y != HEIGHT:
 			bullet3.move_ip(0, 7)
-		if bullet3.y >= WIDTH:
+		if bullet3.y >= HEIGHT:
 			bullet3.x = enemy2_hitbox.x + 20
 			bullet3.y = enemy2_hitbox.y + 100
 		if bullet3.colliderect(plane_hitbox) or bullet3.colliderect(plane_hitbox_y):
@@ -263,9 +338,9 @@ def shoot_player():
 			if health == 0:
 				move = False
 	if score >= 10:
-		if bullet4.y != WIDTH + 20:
+		if bullet4.y != HEIGHT:
 			bullet4.move_ip(0, 6)
-		if bullet4.y >= WIDTH:
+		if bullet4.y >= HEIGHT:
 			bullet4.x = enemy3_hitbox.x + 20
 			bullet4.y = enemy3_hitbox.y + 100
 		if bullet4.colliderect(plane_hitbox) or bullet4.colliderect(plane_hitbox_y):
@@ -283,33 +358,45 @@ def kill_enemy():
 	if move == True:
 		if bullet2.y != -35:
 			bullet2.move_ip(0, -5)
-	if bullet2.y == -20:
-		bullet2.x = plane_hitbox.x  
-		bullet2.y = plane_hitbox.y  
-	if bullet2.colliderect(enemy1_hitbox):
-		pygame.mixer.Sound.play(hitsound)
-		pygame.mixer.music.stop()
-		score += 1  
-		bullet2.x = plane_hitbox.x  
-		bullet2.y = plane_hitbox.y 
-		enemy1_hitbox.x = random.randint(30, WIDTH - 30)
-		enemy1_hitbox.y = -350
-	if bullet2.colliderect(enemy2_hitbox):
-		pygame.mixer.Sound.play(hitsound)
-		pygame.mixer.music.stop()
-		score += 1  
-		bullet2.x = plane_hitbox.x  
-		bullet2.y = plane_hitbox.y 
-		enemy2_hitbox.x = random.randint(30, WIDTH - 30)
-		enemy2_hitbox.y = -350
-	if bullet2.colliderect(enemy3_hitbox):
-		pygame.mixer.Sound.play(hitsound)
-		pygame.mixer.music.stop()
-		score += 1  
-		bullet2.x = plane_hitbox.x  
-		bullet2.y = plane_hitbox.y 
-		enemy3_hitbox.x = random.randint(30, WIDTH - 30)
-		enemy3_hitbox.y = -350
+		if bullet2.y == -20:
+			bullet2.x = plane_hitbox.x  
+			bullet2.y = plane_hitbox.y  
+		if bullet2.colliderect(enemy1_hitbox):
+			pygame.mixer.Sound.play(hitsound)
+			pygame.mixer.music.stop()
+			score += 1  
+			bullet2.x = plane_hitbox.x  
+			bullet2.y = plane_hitbox.y 
+			enemy1_hitbox.x = random.randint(30, WIDTH - 30)
+			enemy1_hitbox.y = -250
+		if bullet2.colliderect(enemy2_hitbox):
+			pygame.mixer.Sound.play(hitsound)
+			pygame.mixer.music.stop()
+			score += 1  
+			bullet2.x = plane_hitbox.x  
+			bullet2.y = plane_hitbox.y 
+			enemy2_hitbox.x = random.randint(30, WIDTH - 30)
+			enemy2_hitbox.y = -450
+		if bullet2.colliderect(enemy3_hitbox):
+			pygame.mixer.Sound.play(hitsound)
+			pygame.mixer.music.stop()
+			score += 1  
+			bullet2.x = plane_hitbox.x  
+			bullet2.y = plane_hitbox.y 
+			enemy3_hitbox.x = random.randint(30, WIDTH - 30)
+			enemy3_hitbox.y = -150
+		if key[pygame.K_a]:
+			bullet2.move_ip(5, 0)
+			second_bullet.move_ip(5, 0)
+		if key[pygame.K_d]:
+			bullet2.move_ip(-5, 0)
+			second_bullet.move_ip(-5, 0)
+		if key[pygame.K_LEFT]:
+			bullet2.move_ip(5, 0)
+			second_bullet.move_ip(5, 0)
+		if key[pygame.K_RIGHT]:
+			bullet2.move_ip(-5, 0)
+			second_bullet.move_ip(-5, 0)
 
 def enemy_stay():
 	if enemy1_hitbox.x >= WIDTH:
@@ -324,19 +411,27 @@ def enemy_stay():
 		enemy3_hitbox.x = 10
 	if enemy3_hitbox.x <= 0:
 		enemy3_hitbox.x = WIDTH -50
+	if bullet2.x >= WIDTH:
+		bullet2.x = 10  
+	if bullet2.x <= 0:
+		bullet2.x = WIDTH
+	if second_bullet.x >= WIDTH:
+		second_bullet.x = 10  
+	if second_bullet.x <= 0:
+		second_bullet.x = WIDTH
 
 pygame.init()
-WIDTH = 1050
-HEIGHT = 800
+WIDTH = 750
+HEIGHT = 1000
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 FPS = 60
 clock = pygame.time.Clock()
-pygame.display.set_caption('Plain Plane Game: 1.4')
+pygame.display.set_caption('Plain Plane Game')
 plane = pygame.image.load('assets/plane.png')
 plane = pygame.transform.scale(plane, (250, 200))
 plane = pygame.transform.rotate(plane, 270)
-plane_hitbox = pygame.Rect(WIDTH / 2 - 10, HEIGHT / 2 + 110, 20, 200)
-plane_hitbox_y = pygame.Rect(plane_hitbox.x - 65, plane_hitbox.y + 110, 150, 20)
+plane_hitbox = pygame.Rect(WIDTH / 2 - 10, HEIGHT - 280, 20, 200)
+plane_hitbox_y = pygame.Rect(plane_hitbox.x - 50, plane_hitbox.y + 110, 125, 20)
 lake = pygame.Rect(random.randint(0, WIDTH), random.randint(0, HEIGHT), 50, 50)
 lake_tex = pygame.image.load('assets/lake.png')
 lake_tex = pygame.transform.scale(lake_tex, (200, 200))
@@ -361,17 +456,17 @@ move = False
 font1 = pygame.font.SysFont('comicsans', 100)
 enemy1 = pygame.image.load('assets/enemy1.png')
 enemy1 = pygame.transform.scale(enemy1, (250, 250))
-enemy1_hitbox = pygame.Rect(random.randint(30, WIDTH - 30), -400, 50, 200)
+enemy1_hitbox = pygame.Rect(random.randint(30, WIDTH - 30), -400, 75, 210)
 enemy2 = pygame.image.load('assets/enemy2.png')
 enemy2 = pygame.transform.scale(enemy2, (250, 250))
-enemy2_hitbox = pygame.Rect(random.randint(30, WIDTH - 30), -400, 50, 200)
+enemy2_hitbox = pygame.Rect(random.randint(30, WIDTH - 30), -400, 75, 210)
 enemy3 = pygame.image.load('assets/enemy3.png')
 enemy3 = pygame.transform.scale(enemy3, (250, 250))
-enemy3_hitbox = pygame.Rect(random.randint(30, WIDTH - 30), -400, 50, 200)
-bullet = pygame.Rect(enemy1_hitbox.x + 20, enemy1_hitbox.y + 100, 15, 15)
-bullet2 = pygame.Rect(plane_hitbox.x, plane_hitbox.y, 15, 15) 
-bullet3 = pygame.Rect(enemy2_hitbox.x + 20, enemy2_hitbox.y + 100, 15, 15)
-bullet4 = pygame.Rect(enemy3_hitbox.x + 20, enemy3_hitbox.y + 100, 15, 15)
+enemy3_hitbox = pygame.Rect(random.randint(30, WIDTH - 30), -400, 75, 210)
+bullet = pygame.Rect(enemy1_hitbox.x + 20, enemy1_hitbox.y + 100, 17, 17)
+bullet2 = pygame.Rect(plane_hitbox.x, plane_hitbox.y, 17, 17) 
+bullet3 = pygame.Rect(enemy2_hitbox.x + 20, enemy2_hitbox.y + 100, 17, 17)
+bullet4 = pygame.Rect(enemy3_hitbox.x + 20, enemy3_hitbox.y + 100, 17, 17)
 font2 = pygame.font.SysFont('8bit', 80)
 score: abs = 0
 paused = False
@@ -383,9 +478,9 @@ empty_heart = pygame.transform.scale(empty_heart, (50, 50))
 hitsound = pygame.mixer.Sound('audioassets/hitmarker.wav')
 highscore = 0  
 real_highscore = 0  
-#run_music = pygame.mixer.music.load('audioassets/runninginthe.wav')
 main_text = pygame.image.load('assets/main_text.png')
-main_text = pygame.transform.scale(main_text, (1000, 800))
+main_text = pygame.transform.scale(main_text, (800, 800))
+second_bullet = pygame.Rect(plane_hitbox.x, plane_hitbox.y, 17, 17)
 
 run = True
 while run:
@@ -398,10 +493,6 @@ while run:
 				move = True 
 				health = 3
 				score = 0
-			if event.key == pygame.K_ESCAPE:
-				paused = True
-			elif event.key == pygame.K_ESCAPE:
-				paused = False
 
 	clock.tick(FPS)
 	plan_hitboxs = pygame.draw.rect(screen, 'red', plane_hitbox), pygame.draw.rect(screen, 'red', plane_hitbox_y)
@@ -430,41 +521,39 @@ while run:
 		screen.blit(empty_heart, (95, HEIGHT - 80))
 	pygame.draw.rect(screen, 'gainsboro', cloud_p1), pygame.draw.rect(screen, 'gainsboro', cloud_p2)
 	pygame.draw.rect(screen, 'gainsboro', cloud2_p1), pygame.draw.rect(screen, 'gainsboro', cloud2_p2)
-	pygame.draw.ellipse(screen, 'indianred3', bullet)
-	pygame.draw.ellipse(screen, 'indianred3', bullet3)
-	screen.blit(enemy3, (enemy3_hitbox.x - 125, enemy3_hitbox.y))
-	screen.blit(enemy1, (enemy1_hitbox.x - 125, enemy1_hitbox.y))
-	screen.blit(enemy2, (enemy2_hitbox.x - 125, enemy2_hitbox.y))
-	pygame.draw.ellipse(screen, 'indianred3', bullet2)
-	pygame.draw.ellipse(screen, 'indianred3', bullet4)
+	pygame.draw.rect(screen, 'indianred3', bullet)
+	pygame.draw.rect(screen, 'indianred3', bullet3)
+	screen.blit(enemy3, (enemy3_hitbox.x - 115, enemy3_hitbox.y))
+	screen.blit(enemy1, (enemy1_hitbox.x - 115, enemy1_hitbox.y))
+	screen.blit(enemy2, (enemy2_hitbox.x - 115, enemy2_hitbox.y))
+	pygame.draw.rect(screen, 'indianred3', bullet2)
+	pygame.draw.rect(screen, 'indianred3', bullet4)
+	pygame.draw.rect(screen, 'indianred3', second_bullet)
 	if move == False:
 		screen.fill('light green')
 		##3 place main_text here
-		screen.blit(main_text, (0, 0))
+		screen.blit(main_text, (-45, -15))
 		highscore = highscore + score
 		if highscore > real_highscore:
 			real_highscore = 0
 			real_highscore = real_highscore + highscore 
 		highscore_text = font1.render(f'SCORE {highscore}', True, 'darkturquoise')
 		real_highscore_text = font2.render(f'HIGHSCORE {real_highscore}', True, 'darkturquoise')
-		screen.blit(highscore_text, (WIDTH / 2 + 150, HEIGHT / 2 + 200))
-		screen.blit(real_highscore_text, (WIDTH / 2 + 55, HEIGHT / 2 + 300))
+		screen.blit(highscore_text, (WIDTH / 2 + 15, HEIGHT / 2 + 200))
+		screen.blit(real_highscore_text, (WIDTH / 2 - 80, HEIGHT / 2 + 300))
 		score = 0  
 	else:
 		highscore = 0  
 		pass
-	screen.blit(plane, (WIDTH / 2 - 100, HEIGHT / 2 + 100))
-	if paused == True:
-		screen.fill('aquamarine')
+	screen.blit(plane, (WIDTH / 2 - 100, HEIGHT - 300))
 
-	if move == True:
-		move_background()
+	move_background()
 	reset_to_top()
 	enemy_movement()
 	shoot_player()
 	kill_enemy()
 	enemy_stay()
-#	music_start()
+	bullet_two()
 
 	pygame.display.flip()
 	pygame.display.update()
